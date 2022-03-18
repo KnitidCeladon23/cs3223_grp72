cae1215bb3bfc6843347c26d72a6b8c09d1a8073
KnitidCeladon23 <kenny.chewpuyang23@gmail.com>
2022-03-18 14:58:52 +0800

    implement hashjoinplan and hashjoinscan

---
+++ SimpleDBEngine/src/simpledb/materialize/HashJoinPlan.java
@@ -0,0 +1,57 @@
+package simpledb.materialize;
+
+import java.util.List;
+
+import simpledb.plan.Plan;
+import simpledb.query.Scan;
+import simpledb.record.Schema;
+import simpledb.tx.Transaction;
+
+public class HashJoinPlan implements Plan {
+	
+	private Transaction tx;
+	private Plan p1, p2;
+	private String fldname1, fldname2;
+	private Schema sch = new Schema();
+	
+	private int numOfPartitions;
+	
+	public HashJoinPlan(Transaction tx, Plan p1, Plan p2, String fldname1, String fldname2) {
+		this.tx = tx;
+		this.p1 = p1;
+		this.p2 = p2;
+		this.fldname1 = fldname1;
+		this.fldname2 = fldname2;
+		sch.addAll(p1.schema());
+		sch.addAll(p2.schema());
+		this.numOfPartitions = tx.availableBuffs() - 1;
+	}
+	
+	public Scan open() {
+		Scan s1 = p1.open();
+		Scan s2 = p2.open();
+		List<String> s1Fields = p1.schema().fields();
+		return new HashJoinScan(s1, s2, fldname1, fldname2, s1Fields, numOfPartitions);
+	}
+	
+	public int blocksAccessed() {
+		return 3 * (p1.blocksAccessed() + p2.blocksAccessed());
+	}
+	
+	public int recordsOutput() {
+		return p1.recordsOutput() * p2.recordsOutput();
+	}
+	
+	public int distinctValues(String fldname) {
+		if (p1.schema().hasField(fldname)) {
+			return p1.distinctValues(fldname);
+		} else {
+			return p2.distinctValues(fldname);
+		}
+	}
+	
+	public Schema schema() {
+		return sch;
+	}
+
+}
---
+++ SimpleDBEngine/src/simpledb/materialize/HashJoinScan.java
@@ -0,0 +1,123 @@
+package simpledb.materialize;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import simpledb.query.Constant;
+import simpledb.query.Scan;
+
+public class HashJoinScan implements Scan {
+
+	private Scan s1, s2;
+	private String fldname1, fldname2;
+	private List<String> s1Fields;
+	private int numOfPartitions;
+	
+	private int curr_index, curr_partition;
+	private Map<String, Constant> curr_s1Record;
+	private ArrayList<Map<String, Constant>> curr_arr;
+	private Map<Constant, ArrayList<Map<String, Constant>>> hashTable;
+	
+	public HashJoinScan(Scan s1, Scan s2, String fldname1, String fldname2, List<String> s1Fields, int numOfPartitions) {
+		this.s1 = s1;
+		this.s2 = s2;
+		this.fldname1 = fldname1;
+		this.fldname2 = fldname2;
+		this.s1Fields = s1Fields;
+		this.numOfPartitions = numOfPartitions;
+		this.curr_index = 0;
+		
+		beforeFirst();
+	}
+	
+	public void close() {
+		s1.close();
+		s2.close();
+	}
+	
+	public void beforeFirst() {
+		
+	}
+	
+	public boolean next() {
+		
+		while (true) {
+			
+			// if curr_arr has already been initialised and there are still unmatched records
+			if (curr_arr != null && curr_index < curr_arr.size()) {
+				curr_s1Record = curr_arr.get(curr_index);
+				curr_index++;
+				return true;
+			}
+			
+			//iterating through s2
+			while(s2.next()) {
+				// Pass s2 value into hash function and match against hash table.
+				if(curr_partition == (s2.getVal(fldname2).hashCode() % numOfPartitions)) {
+					if(hashTable.keySet().contains(s2.getVal(fldname2))) {
+						curr_index = 0;
+						curr_arr = hashTable.get(s2.getVal(fldname2));
+						curr_s1Record = curr_arr.get(curr_index);
+						curr_index++;
+						return true;
+					}
+				}
+			}
+			curr_arr = null;
+			
+			// checking if there is next partition
+			curr_partition++;
+			if(curr_partition >= numOfPartitions) {
+				return false;
+			}
+			
+			s1.beforeFirst();
+			s2.beforeFirst();
+			hashTable = new HashMap<Constant, ArrayList<Map<String, Constant>>>();
+
+			//iterating through s1
+			while(s1.next()) {
+				if(curr_partition == s1.getVal(fldname1).hashCode() % numOfPartitions) {
+					Map<String, Constant> curr_record = new HashMap<>();
+					for(String curr_fieldName: s1Fields) {
+						curr_record.put(curr_fieldName, s1.getVal(curr_fieldName));
+					}
+					hashTable.computeIfAbsent(s1.getVal(fldname1), k -> new ArrayList<Map<String, Constant>>()).add(curr_record);
+				}
+			}
+			
+			return true;
+		}
+		
+	}
+	
+	public int getInt(String fldname) {
+		if (s2.hasField(fldname)) {
+			return s2.getInt(fldname);
+		} else { 
+			return curr_s1Record.get(fldname).asInt();
+		}
+	}
+	
+	public String getString(String fldname) {
+		if (s2.hasField(fldname)) {
+			return s2.getString(fldname);
+		} else {
+			return curr_s1Record.get(fldname).asString();
+		}
+	}
+	
+	public Constant getVal(String fldname) {
+		if (s2.hasField(fldname)) {
+			return s2.getVal(fldname);
+		} else {
+			return curr_s1Record.get(fldname);
+		}
+	}
+	
+	public boolean hasField(String fldname) {
+		return s2.hasField(fldname) || curr_s1Record.containsKey(fldname);
+	}
+}
